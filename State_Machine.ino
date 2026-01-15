9b06151b09321f98104f304f1c5980661390897e
Turtlerock0010 <danielrprincipe@gmail.com>
2026-01-15 11:03:36 -0500

    Add files via upload

---
+++ State_Machine.ino
@@ -0,0 +1,11 @@
+// REPLACE ALL OF "<Desired Button Here>" WITH THE CORRECT BUTTON
+// All angles are a copy of the start position of the parts
+if (PestoLink.buttonHeld("<Desired Button Here>")) {
+  // L1
+} else if (PestoLink.buttonHeld("<Desired Button Here>")) {
+  // L2
+} else if (PestoLink.buttonHeld("<Desired Button Here>")) {
+  // L3
+} else if (PestoLink.buttonHeld("<Desired Button Here>")) {
+  // L4
+}
